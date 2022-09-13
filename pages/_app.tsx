9f8e5928700c96e4ee09a4e9be27e9b1c7c7a2ec
Adam Akbar <adam.akbar@ruangguru.com>
2022-09-13 22:31:20 +0700

    feat(ui): setup apollo and chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const client = new ApolloClient({
+    uri: 'https://beta.pokeapi.co/graphql/v1beta',
+    cache: new InMemoryCache({}),
+  });
+
+  return (
+    <ChakraProvider>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
